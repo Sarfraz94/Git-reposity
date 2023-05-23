07164679e365031c48f7f535f1b76ebfb7ce9130
sarfraz <sarfrazali43205@gmail.com>
2023-05-23 04:16:31 -0700

    include odd even program

---
+++ odd-even.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class index{
+public static void main (Sting[] arg){
+Scanner c1=new Scanner (System.in);
+int input =c1.nextInt();
+if (input%2=0){
+System.out.println("even");
+}
+else{
+System.out.println("odd");
+}
+}
