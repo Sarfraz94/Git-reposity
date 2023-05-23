ecaa6418ca314c83448b5a04a436b2d3b5fc1c4a
sarfraz <sarfrazali43205@gmail.com>
2023-05-23 04:30:29 -0700

    insert another program

---
+++ fibonic.java
@@ -0,0 +1,17 @@
+import java.util.*;
+claas a{
+public static void main (String[] arg){
+Scanner=ob=new Scanner(System.in);
+int a=0;
+int b=1;
+int c;
+System.out println(" term repetation");
+int terms=ob.nextInt();
+for(int i=1;i<=terms;i++){
+System.out.print(a+ ","):
+c=a+b;
+a+b;
+b=c;
+}
+}
+}
